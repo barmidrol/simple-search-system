366c7dec7dc09883b5d617d2c7f5d9c0ecec1233
Siarhei Padlozny <barmidrol@gmail.com>
2016-05-08 01:35:39 +0300

    Add .gitignore

---
+++ search_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SearchSystemConfig(AppConfig):
+    name = 'search_system'
--- simple_search_system/settings.py
+++ simple_search_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'search_system',
 ]
 
 MIDDLEWARE_CLASSES = [
